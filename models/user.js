33cbb0adc748d7e43682152772969cc4de7d226f
Upendra12395 <ch95upendra@gmail.com>
2021-11-08 18:20:08 +0530

    user model

--- models/user.js
+++ models/user.js
@@ -5,18 +5,29 @@ const userSchema = mongoose.Schema({
         type : String,
         required: true
     },
-    contact:{
+    phone:{
         type: Number,
-        required : true
-    },
-    email:{
-        type: String,
+        match: [/^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/, 'Please fill a valid phone no'],
         required: true
+    },
+    email: {
+        type: String,
+        //validate: [ isEmail, 'invalid email' ]
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     password:{
         type: String,
         required: true
+    },
+    orderList:[{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Order"
+    }],
+    loggedInAt:{
+        type : Date
     }
 },{timestamps: true})
 
 module.exports = mongoose.model("User", userSchema)
+
+
