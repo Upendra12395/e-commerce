f4c98e09febc03f3983b3ec28795b296b3edd1e7
Upendra12395 <ch95upendra@gmail.com>
2021-11-08 18:30:37 +0530

    order model

---
+++ models/order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const orderSchema = mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    product : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Product"
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    address : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Address'
+    }
+})
+
+module.exports = mongoose.model("Order", orderSchema)
