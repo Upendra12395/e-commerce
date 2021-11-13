a163963355b38141e1e2232f65b7028fbb8e9b37
Upendra12395 <ch95upendra@gmail.com>
2021-11-13 22:11:48 +0530

    controller to show all address related a user

--- controllers/address.js
+++ controllers/address.js
@@ -66,5 +66,12 @@ module.exports.deleteOne = (req, res)=>{
 }
 
 module.exports.showAll = (req, res)=>{
-    
+    const userId = req.user._id
+    Address.find({user : userId})
+    .then((address)=>{
+        res.status(200).json(address)
+    })
+    .catch(err =>{
+        res.status(500).json({message : err.message})
+    })
 }
