c4b76b985318bcf4ac8df340b89cd26104ab507c
Upendra12395 <ch95upendra@gmail.com>
2021-11-15 21:32:08 +0530

    order controller

---
+++ controllers/order.js
@@ -0,0 +1,13 @@
+const Order = require('../models/order')
+
+module.exports.makeOrder = (req, res)=>{
+
+}
+
+module.exports.cancelOrder = (req, res)=>{
+
+}
+
+module.exports.changeDeliveryaddress = (req, res)=>{
+    
+}
